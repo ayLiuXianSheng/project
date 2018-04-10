ecc27e88421a478f249456b74f37125815dd8ee4
Author Name <Author Email>
2018-04-10 14:12:54 +0800

    代码

--- JD/app/src/main/java/com/example/lenovo/jd/view/api/Api.java
+++
@@ -1,9 +0,0 @@
-package com.example.lenovo.jd.view.api;
-
-/**
- * Created by lenovo on 2018/4/9.
- */
-
-public class Api {
-    public static final String HOME_NAME = "https://www.zhaoapi.cn/";
-}
